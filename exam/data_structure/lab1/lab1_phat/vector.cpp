797b12c2986b4de60fa176226dc0350dd73546cc
phatnguyen02 <ngphat1211@gmail.com>
2021-06-28 21:34:35 +0700

    lab1_phat_vector: sửa hàm nhập

--- exam/data_structure/lab1/lab1_phat/vector.cpp
+++ exam/data_structure/lab1/lab1_phat/vector.cpp
@@ -3,23 +3,23 @@
 #include <vector>
 #include <sstream>
 using namespace std;
-void enter(vector<string> &arr, int &n);
+void enter(vector<string> &arr);
 void print(vector<string> arr);
 int main(){
     vector<string> list;
-    int n;
-    enter(list, n);
+    enter(list);
     print(list);
 }
-void enter(vector<string> &arr, int &n){
-    string a[100];
+void enter(vector<string> &arr){
+    int n;
     cout<<"Enter n: ";
     cin>>n;
-    for(int i=0; i<n;i++){
-        cout<<"Enter animal "<<i+1<<endl;
+    while(n--){
+        string a;
+        cout<<"Enter animal "<<n<<endl;
         fflush(stdin);
-        getline(cin, a[i]);
-        arr.push_back(a[i]);
+        getline(cin, a);
+        arr.push_back(a);
     }
 }
 void print(vector<string> arr){
