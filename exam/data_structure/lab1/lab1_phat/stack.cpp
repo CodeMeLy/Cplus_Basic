38b628615f466e6584056fee846803c0e5a1935d
phatnguyen02 <ngphat1211@gmail.com>
2021-06-29 10:11:39 +0700

    lab1_phat_stack: thêm và xóa 1 đĩa nhạc trong ds

--- exam/data_structure/lab1/lab1_phat/stack.cpp
+++ exam/data_structure/lab1/lab1_phat/stack.cpp
@@ -3,10 +3,16 @@
 #include <string>
 using namespace std;
 void enter(stack<string> &arr);
+void add(stack<string> &arr);
+void remove(stack<string> &arr);
 void print(stack<string> arr);
 int main(){
     stack<string> list;
     enter(list);
+    print(list);
+    add(list);
+    print(list);
+    remove(list);
     print(list);
 }
 void enter(stack<string> &arr){
@@ -15,15 +21,26 @@ void enter(stack<string> &arr){
     cout<<"Enter n: ";
     cin>>n;
     while(n--){
-        cout<<"Enter music name "<<n<<endl;
+        cout<<"Enter disc "<<n<<endl;
         fflush(stdin);
         getline(cin, name);
         arr.push(name);
     }
+}
+void add(stack<string> &arr){
+    string name;
+    cout<<"Enter disc: ";
+    fflush(stdin);
+    getline(cin, name);
+    arr.push(name);
+}
+void remove(stack<string> &arr){
+    arr.pop();
 }
 void print(stack<string> arr){
     while(!arr.empty()){
         cout<<arr.top()<<" ";
         arr.pop();
     }
+    cout<<endl;
 }
